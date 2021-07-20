8914cfc8119ba0f70e1c5d4131d0c0f8f8361c1e
Your Name <you@example.com>
2021-07-20 19:22:39 +0600

    while loop and do while loop in dart

---
+++ Whileloop.dart
@@ -0,0 +1,18 @@
+void main(){
+  
+
+var i=0;
+
+
+   /**  while(i<10){   // while  loop
+     print(i);
+     i++;
+ }
+**/
+
+ do{
+     print(i);  // do while loop
+     i++;
+ }while(i<10);
+
+}
